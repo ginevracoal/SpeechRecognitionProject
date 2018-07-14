d1dab3656b40b08dd8d3b6c548cf27843ec3a5e4
Francesco Franchina <cescus92@gmail.com>
2018-07-14 22:03:45 +0200

    Completed function to read files (dataset)

--- src/train.py
+++ src/train.py
@@ -1,8 +1,30 @@
+import os
 from sklearn.model_selection import train_test_split
 
+import utils
 
 def load_data(dirname):
-    pass
+    print('Loading data from the filesystem ({})'.format(dirname))
+
+    dataset = {'x': [], 'y': []}
+    for root, dirs, files in os.walk(dirname):
+        currentdir = os.path.basename(root)
+        if not currentdir.startswith('_'):
+            for filename in files:
+                if filename.endswith('.wav'):
+                    dataset['x'].append(os.path.join(root, filename))
+                    dataset['y'].append(currentdir)
+
+    print('Loaded {} samples'.format(len(dataset['x'])))
+
+    classes = set(dataset['y'])
+    print(len(classes), 'classes found: ', ', '.join(classes))
+
+    print('Converting classes to integers')
+    dataset['y'] = [utils.class2int_map[x] for x in dataset['y']]
+
+    return dataset
+
 
 def build_model():
     pass
@@ -13,7 +35,7 @@ def train(model, x, y, checkpoint_every=100):
 def validate(model, x, y):
     pass
 
-dataset = load_data('~/speech')
+dataset = load_data('/galileo/home/userexternal/ffranchi/speech')
 
 seed = 44
 train_x, test_x, train_y, test_y = \
