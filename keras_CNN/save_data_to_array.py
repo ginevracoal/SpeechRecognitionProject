73080207c57f93b93df060e2501efd84462b48d0
Ginevra <ginevracoal@gmail.com>
2018-07-16 18:37:25 +0200

    CNN is working

---
+++ keras_CNN/save_data_to_array.py
@@ -0,0 +1,9 @@
+# I only run this once
+
+from preprocess import * 
+
+# Second dimension of the feature is dim2
+feature_dim_2 = 11
+
+# Save data to array file first
+save_data_to_array(max_len=feature_dim_2)
