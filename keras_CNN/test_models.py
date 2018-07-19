57ffd58362c4cd2b1496faecd3a591d0860e9d34
Ginevra <ginevracoal@gmail.com>
2018-07-19 09:20:25 +0200

    updated script for model testing

--- keras_CNN/test_models.py
+++ keras_CNN/test_models.py
@@ -1,3 +1,6 @@
+## EXAMPLE: go to the main folder and run
+## python group/keras_CNN/test_models.py model_1 > group/keras_CNN/trained_models/model_1.out
+
 from preprocess import *
 from CNN_models import *
 import keras
@@ -6,16 +9,17 @@ from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.utils import to_categorical
 from keras.models import model_from_json
 
+import sys
 
 DATA_PATH = "/galileo/home/userexternal/gcarbone/group/data/speech/"
 out_dir = "/galileo/home/userexternal/gcarbone/group/keras_CNN/arrays/"
 
 labels, _, _ = get_labels()
 
-# # Loading train set and test set
+## Loading train set and test set
 X_train, X_test, y_train, y_test = get_train_test()
 
-# # Feature dimension
+## Feature dimension
 feature_dim_1 = 20
 feature_dim_2 = 11
 channel = 1
@@ -24,20 +28,29 @@ batch_size = 100
 verbose = 1
 num_classes = len(labels) # 35 without background noise
 
-# Reshaping to perform 2D convolution, which is spatial convolution over images
+## Reshaping to perform 2D convolution, which is spatial convolution over images
 X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
 X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
 
 y_train_hot = to_categorical(y_train)
 y_test_hot = to_categorical(y_test)
 
-model_1 = model_1()
-model_1.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot))
-save_model(model_1,'model_1')
+## taking a subset to test the job
+# n = 100
+# X_train = X_train[:n]
+# X_test = X_test[:n//2]
+# y_train_hot = y_train_hot[:n]
+# y_test_hot = y_test_hot[:n//2]
 
-model_2 = model_1()
-model_2.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot))
-save_model(model_2,'model_2')
+## with this piece of code I can give the name as an input both from command line and as console input ;)
+if __name__ == "__main__":
+		try:
+				model_name = sys.argv[1]
+		except IndexError:
+				model_name = input("\nPlease give the name of the model: ")
 
+print("\nYou entered ", model_name)
 
-
+model = eval(model_name)()
+model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot))
+save_model(model, model_name)
--- keras_CNN/test_models.sh
+++ keras_CNN/test_models.sh
@@ -7,5 +7,6 @@
 #SBATCH --partition=gll_usr_gpuprod
 #SBATCH --account=uts18_bortoldl_0
 
-. virtual_jupyter/bin/activate
-python /galileo/home/userexternal/gcarbone/group/keras_CNN/test_models.py
+. source virtualenv_2/bin/activate
+python /galileo/home/userexternal/gcarbone/group/keras_CNN/test_models.py model_1 > group/keras_CNN/trained_models/model_1.out
+python /galileo/home/userexternal/gcarbone/group/keras_CNN/test_models.py model_2 > group/keras_CNN/trained_models/model_2.out
