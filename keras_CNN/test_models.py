ea6b10eb46fa88e989af4a409e1ad524726a6edf
Ginevra <ginevracoal@gmail.com>
2018-07-25 11:32:40 +0200

    it works!

--- keras_CNN/test_models.py
+++ keras_CNN/test_models.py
@@ -19,38 +19,40 @@ out_dir = "/galileo/home/userexternal/gcarbone/group/keras_CNN/arrays_32/"
 labels, _, _ = get_labels()
 
 ## Loading train set and test set
-X_train, X_test, y_train, y_test = get_train_test()
+X, X_test, y, y_test = get_train_test()
 
-# taking a subset to test the job
-n = 300
-X_train = X_train[:n]
-X_test = X_test[:n//2]
-y_train = y_train[:n]
-y_test = y_test[:n//2]
+# # taking a subset to test the job
+# n = 300
+# X = X[:n]
+# X_test = X_test[:n//2]
+# y = y[:n]
+# y_test = y_test[:n//2]
 
 ## Feature dimension
 feature_dim_1 = 32
 feature_dim_2 = 32
 channel = 1
-epochs = 1
+epochs = 20
 batch_size = 100
 verbose = 1
 num_classes = len(labels) # 35 without background noise
 
 ## Reshaping to perform 2D convolution, which is spatial convolution over images
-X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
+X = X.reshape(X.shape[0], feature_dim_1, feature_dim_2, channel)
 X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
-y_train_hot = to_categorical(y_train)
+y_hot = to_categorical(y)
 y_test_hot = to_categorical(y_test)
 
 # split train into train and validation
-train_len = len(X_train)
-X_train = X_train[:train_len//2]
-X_validation = X_train[train_len//2:]
-y_train_hot = y_train_hot[:train_len//2]
-y_validation_hot =  y_train_hot[train_len//2:]
+train_len = len(X)//2
 
-print(X_train.shape, y_train.shape)
+X_train = X[:train_len]
+y_train_hot = y_hot[:train_len]
+
+X_validation = X[train_len:]
+y_validation_hot = y_hot[train_len:]
+
+print(X_train.shape, y_train_hot.shape)
 print(X_validation.shape, y_validation_hot.shape)
 
 
@@ -67,26 +69,29 @@ model = eval(model_name)()
 model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_validation, y_validation_hot))
 save_model(model, model_name)
 
-
 ############ make real predictions
 
-df = pd.read_csv(DATA_PATH+'testing_list.txt', delimiter=',')
-testing_list = df.iloc[:,0]
-testing_list = [os.path.join(DATA_PATH,filename) for filename in testing_list]
+score = model.evaluate(X_test, y_test_hot)
+print(score)
 
-# Predicts one sample given its path
-def predict(filepath, model):
-    sample = wav2mfcc(filepath)
-    sample_reshaped = sample.reshape(1, feature_dim_1, feature_dim_2, channel)
-    return get_labels()[0][np.argmax(model.predict(sample_reshaped))]
 
-def predict_list(testing_list, model):
-    predictions = []
-    for filename in testing_list:
-        predictions.append(predict(filename, model=model))
-    return(predictions)
+# df = pd.read_csv(DATA_PATH+'testing_list.txt', delimiter=',')
+# testing_list = df.iloc[:,0]
+# testing_list = [os.path.join(DATA_PATH,filename) for filename in testing_list]
 
-predictions = predict_list(testing_list, model)
+# # Predicts one sample given its path
+# def predict(filepath, model):
+#     sample = wav2mfcc(filepath)
+#     sample_reshaped = sample.reshape(1, feature_dim_1, feature_dim_2, channel)
+#     return get_labels()[0][np.argmax(model.predict(sample_reshaped))]
 
-## save the results as a dictionary
-json.dump(predictions, open(path + model_name + '_predictions.json','w'))
+# def predict_list(testing_list, model):
+#     predictions = []
+#     for filename in testing_list:
+#         predictions.append(predict(filename, model=model))
+#     return(predictions)
+
+# predictions = predict_list(testing_list, model)
+
+# ## save the results as a dictionary
+# json.dump(predictions, open(path + model_name + '_predictions.json','w'))
